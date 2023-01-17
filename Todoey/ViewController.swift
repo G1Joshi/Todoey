efc29799d0b7c26aad60fb6cb255b38329f19dfd
Jeevan Joshi <joshijeevan98@gmail.com>
2023-01-17 13:34:46 +0530

    initial commit

---
+++ Todoey/ViewController.swift
@@ -0,0 +1,15 @@
+//
+//  ViewController.swift
+//  Todoey
+//
+//  Created by Jeevan Chandra Joshi on 17/01/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemBackground
+    }
+}
